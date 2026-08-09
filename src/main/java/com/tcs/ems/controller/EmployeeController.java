8971a01a190955fd0234e8cabd716273626ac39c
ManjunathKesarur <manjunath.dev05@gmail.com>
2026-08-10 00:08:20 +0530

    Add Employee entity, repo, service & controller
    Introduce basic Employee CRUD: add JPA entity (Employee) with validation and Lombok, a JpaRepository (EmployeeRepository), a service (EmployeeService) implementing create/read/update/delete operations, and a REST controller (EmployeeController) exposing endpoints at /employees (POST, GET, GET/{email}, PUT, DELETE/{email}). This is an initial implementation to wire persistence and HTTP API for employee management.

---
+++ src/main/java/com/tcs/ems/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.tcs.ems.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcs.ems.entity.Employee;
+import com.tcs.ems.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public EmployeeController(@RequestBody EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	@PostMapping
+	public String cretaeEmployee(@RequestBody Employee employee) {
+		return employeeService.cretaeEmployee(employee);
+	}
+	
+	@GetMapping("/{email}")
+	public Object fetchEmployeeByEmail(@PathVariable String email) {
+	return employeeService.fetchEmployeeByEmail(email);
+	}
+	
+	@GetMapping
+	public List<Employee> fetchAllEmployees(){
+	return employeeService.fetchAllEmployees();
+	}
+	
+	@DeleteMapping("/{email}")
+	public String deleteEmployeeByEmail(@PathVariable String email) {
+		return employeeService.deleteEmployeeByEmail(email);
+	}
+	
+	@PutMapping
+	public String updateEmployeeByEmail(@RequestBody Employee employee) {
+		return employeeService.updateEmployeeByEmail(employee);
+	}
+}
