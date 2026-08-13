fd268bb1e429836387424fe55ec3438317983c85
ManjunathKesarur <manjunath.dev05@gmail.com>
2026-08-13 20:09:29 +0530

    Throw on duplicate employee creation
    Add a uniqueness check to cretaeEmployee: the service now looks up an employee by email before saving and throws UserAlreadyExistsException if one already exists, preventing duplicate records. Imported the exception and adjusted method formatting. Other service methods (fetch, fetchAll, delete, update) keep their behavior aside from minor formatting changes.

--- src/main/java/com/tcs/ems/service/EmployeeService.java
+++ src/main/java/com/tcs/ems/service/EmployeeService.java
@@ -6,50 +6,76 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.tcs.ems.entity.Employee;
+import com.tcs.ems.exception.UserAlreadyExistsException;
 import com.tcs.ems.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
 
+	
+	
 	private final EmployeeRepository employeeRepository;
 
+	
 	public EmployeeService(EmployeeRepository employeeRepository) {
 		this.employeeRepository = employeeRepository;
 	}
 	
 	
-public String cretaeEmployee(Employee employee) {
-	employeeRepository.save(employee);
-	return "Employee created successfully";
-}
+	
+	
+	public String cretaeEmployee(Employee employee) {
+		
+		Optional<Employee> oe = employeeRepository.findById(employee.getEmail());
+		if(oe.isEmpty()) {
+		employeeRepository.save(employee);
+		
+			return "Employee created successfully";
+		
+		}else {
+			throw new UserAlreadyExistsException("employee with this email : "+employee.getEmail()+" already exist please enter the unique email to register or delete the old account");
+			}
+	}
+	
+	
+	
+	public Object fetchEmployeeByEmail(String email) {
+		
+		Optional<Employee> oe = employeeRepository.findById(email);
+		if(oe.isPresent()) {
+			return oe.get();
+		}else {
+			return "the entered email is not present in databae";
+		}
+	}
 
-public Object fetchEmployeeByEmail(String email) {
+	
+	
+	public List<Employee> fetchAllEmployees(){
+		
+		return 	employeeRepository.findAll();
+	}
 
-Optional<Employee> oe = employeeRepository.findById(email);
-if(oe.isPresent()) {
-return oe.get();
-}else {
-	return "the entered email is not present in databae";
-}
-}
+	
+	
+	public String deleteEmployeeByEmail(String email) {
 
-public List<Employee> fetchAllEmployees(){
-return 	employeeRepository.findAll();
-}
+		Optional<Employee> oe = employeeRepository.findById(email);
+		if(oe.isPresent()) {
+			employeeRepository.deleteById(email);
+			return "enter email data deleted";
+		}else {
+			return "the entered email is not present in databae";
+		}
+	}
 
-public String deleteEmployeeByEmail(String email) {
-
-Optional<Employee> oe = employeeRepository.findById(email);
-if(oe.isPresent()) {
- employeeRepository.deleteById(email);
- return "enter email data deleted";
-}else {
-	return "the entered email is not present in databae";
-}
-}
-
-public String updateEmployeeByEmail(Employee employee) {
-	employeeRepository.save(employee);
-	return "data updated";
-}
+	
+	
+	public String updateEmployeeByEmail(Employee employee) {
+		employeeRepository.save(employee);
+		return "data updated";
+	}
+	
+	
+	
 }
